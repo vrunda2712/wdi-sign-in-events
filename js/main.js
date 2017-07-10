c1bd9d74db403c5613e396c12b0e2626ad9ae71a
vrunda2712 <angy.v27@gmail.com>
2017-07-10 14:42:47 -0400

    When user presses submit button, added an error class to both input elements

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ document.addEventListener('DOMContentLoaded', function(event) {
   var signIn = document.querySelector('.signin');
   var modal = document.querySelector('.modal');
   var close = document.querySelector('.close');
+  var submit = document.querySelector('.submit');
+  var input = document.querySelectorAll('input');
 
   signIn.addEventListener('click',function(){
       modal.style.display = 'block';
@@ -10,4 +12,10 @@ document.addEventListener('DOMContentLoaded', function(event) {
   close.addEventListener('click',function (){
     modal.style.display = 'none';
   });
+
+  submit.addEventListener('click',function(){
+    for(var i = 0;i < input.length; i++) {
+      input[i].className = 'error';
+    }
+  })
 });
