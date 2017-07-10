7218d443d2fc77a36137601ead97cabe5feea7b5
vrunda2712 <angy.v27@gmail.com>
2017-07-10 14:32:24 -0400

    When user presses close button, fade out the model window

--- js/main.js
+++ js/main.js
@@ -1,8 +1,13 @@
 document.addEventListener('DOMContentLoaded', function(event) {
   var signIn = document.querySelector('.signin');
   var modal = document.querySelector('.modal');
+  var close = document.querySelector('.close');
 
   signIn.addEventListener('click',function(){
       modal.style.display = 'block';
+  });
+
+  close.addEventListener('click',function (){
+    modal.style.display = 'none';
   });
 });
